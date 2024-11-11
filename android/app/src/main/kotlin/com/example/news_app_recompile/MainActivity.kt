e9da057660087f747f7c0dfb49ef0a58ebcbc27d
jhalto <zobayerarmannadim@gmail.com>
2024-11-11 13:38:12 +0600

    Revert "recompile"
    This reverts commit a148bb431228c0212ec4631c7692ada885def72b.

--- android/app/src/main/kotlin/com/example/news_app_recompile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.news_app_recompile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/screens/widgets/common_widgets.dart
+++
@@ -1,3 +0,0 @@
-import 'dart:ui';
-
-final Color backgroundClr = Color(0xffFFF2C5);
